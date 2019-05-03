3808f417d9339546e4a94a85f975edc7313047a6
2tallyork <2tallyork@users.noreply.github.com>
2019-05-02 21:00:05 -0400

    not working yet

--- app/components/App.vue
+++ app/components/App.vue
@@ -1,14 +1,12 @@
 <template>
     <Page>
         <ActionBar title="Welcome to Yellow Bucket!" android:flat="true"/>
-        <TabView tabBackgroundColor="#711537"
-                 androidSelectedTabHighlightColor="#ffffff"
-                 tabTextColor="#999999"
-                 selectedTabTextColor="#ffffff">
+        <TabView tabBackgroundColor="#05386B"
+                 androidSelectedTabHighlightColor="#379683"
+                 tabTextColor="#EDF5E1"
+                 selectedTabTextColor="#EDF5E1">
             <TabViewItem title="Movies">
-                <GridLayout columns="*" rows="*">
-                    <Label class="message" :text="msg" col="0" row="0" textWrap="true" />
-                </GridLayout>
+                <movie-component :movies="movies"></movie-component>
             </TabViewItem>
             <TabViewItem title="Customers">
                 <customer-component :customers="customers"></customer-component>
@@ -25,19 +23,24 @@
 <script>
     import axios from "axios";
     import CustomerComponent from '@/components/CustomerComponent.vue';
-
+    import MovieComponent from '@/components/MovieComponent.vue';
     function Customer({id, name, email, isAdmin}) {
         this.id = parseInt(id);
         this.name = name;
         this.email = email;
         this.isAdmin = isAdmin
     }
-
+    function Movie({id, title,length, description}) {
+        this.id = parseInt(id);
+        this.title = title;
+        this.length = length;
+        this.description = description;
+    }
     export default {
         data() {
             return {
-                msg: 'This is where you put you Movie code!  Good luck!',
-                customers: []
+                customers: [],
+                movies: [],
             }
         },
         methods: {
@@ -48,31 +51,54 @@
         mounted() {
             axios.get("https://codeflare.tech/api/customers").then(result => {
                 result.data.forEach(customer => {
-                    this.customers.push(new Customer(customer));
-                })
-            }, error => {
+                console.log(customer.data);
+            this.customers.push(new Customer(customer));
+        })
+        },
+            error => {
+                console.error(error);
+            }),
+            axios.get("https://codeflare.tech/api/movies").then(result => {
+                result.data.forEach(movie => {
+                console.log(movie.data);
+            this.movies.push(new Movie(movie));
+        })
+        }, error => {
                 console.error(error);
             })
         },
         components: {
-            CustomerComponent
+            MovieComponent,
+            CustomerComponent,
         }
+        /*movieMount() {
+         axios.get("https://codeflare.tech/api/movies").then(result => {
+         result.data.forEach(movie => {
+         console.log(movie.data);
+         this.movies.push(new Movie(movie));
+         })
+         }, error => {
+         console.error(error);
+         })
+         },
+         components: {
+         MovieComponent,
+         CustomerComponent,
+         }*/
     }
 </script>
 
 <style scoped>
     ActionBar {
-        background-color: #666666;
-        color: #fafafa;
+        background-color: #5CDB95;
+        color: #EDF5E1;
     }
-
     .message {
         vertical-align: center;
         text-align: center;
         font-size: 20;
         color: #666666;
     }
-
     .label-text {
         color: #444444;
     }
